b8e380a399d4dac40c83eddcd571444483fcf253
David Puscas <davidpuscas@live.se>
2026-02-11 14:49:32 +0100

    moved the maping of movies to the startpage.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,13 @@
-import type { Movie } from './interfaces/Movie';
-import { mapMovieArray } from './interfaces/Movie';
 import { useLocation } from 'react-router-dom';
 import Header from "./partials/Header";
 import Main from './partials/Main';
 import Footer from './partials/Footer';
-import BootstrapBreakpoints from './parts/BootstrapBreakpoints';
-import { useState, useEffect } from 'react';
+
 
 // turn off when not needed for debugging
 const showBootstrapBreakpoints = true;
 
 export default function App() {
-
-  const [movies, setMovies] = useState<Movie[] | null>(null);
-
-  useEffect(() => {
-    (async () => {
-      setMovies(mapMovieArray(await (await fetch('/api/movies')).json()));
-    })();
-  }, []);
-
 
   // scroll to top when the route changes
   useLocation();
@@ -29,12 +17,6 @@ export default function App() {
     <Header />
     <Main />
     <Footer />
-    {showBootstrapBreakpoints ? <BootstrapBreakpoints /> : null}
 
-    map movie and add al the values wished to execute
-    {movies && movies.map(({ Title, Year, id, Poster }) => <article key={id}>
-      <h3>{Title} ({Year})</h3>
-      <img src={Poster} alt="poster of" />
-    </article>)}
   </>;
 };
---
+++ src/pages/StartPage.tsx
@@ -0,0 +1,27 @@
+import App from '../App';
+import type { Movie } from '../interfaces/Movie';
+import { mapMovieArray } from '../interfaces/Movie';
+import { useState } from 'react';
+import { useEffect } from 'react';
+
+export default function StartPage() {
+
+    const [movies, setMovies] = useState<Movie[] | null>(null);
+
+    useEffect(() => {
+        (async () => {
+            setMovies(mapMovieArray(await (await fetch('/api/movies')).json()));
+        })();
+    }, []);
+    // scroll to top when the route changes
+    App();
+    window.scrollTo({ top: 0, left: 0, behavior: 'instant' });
+
+    //map movie and add al the values wished to execute
+    return <>
+        {movies && movies.map(({ Title, Year, id, Poster }) => <article key={id}>
+            <h3>{Title} ({Year})</h3>
+            <img src={Poster} alt="poster of" />
+        </article>)}
+    </>;
+};
