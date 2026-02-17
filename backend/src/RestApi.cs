1e3822fd89168504db1609eea090b0a521c97cae
Chim-Cham <gustavfransson@hotmail.com>
2026-02-17 09:41:29 +0100

    Changes to RestAPI

--- backend/src/RestApi.cs
+++ backend/src/RestApi.cs
@@ -8,6 +8,47 @@ public static class RestApi
             HttpContext context, string table, JsonElement bodyJson
         ) =>
         {
+            if (table == "customBooking")
+            {
+                var user = Session.Get(context, "user");
+                var body = JSON.Parse(bodyJson.ToString());
+
+                string email;
+                int? userID = null;
+
+                if (user != null)
+                {
+                    email = (string)user.email;
+                    userID = (int)user.id;
+                }
+                else
+                {
+                    if (body.email == null)
+                    {
+                        return RestResult.Parse(context, new { error = "Email is required."});
+                    }
+                    email = {string}body.email;
+                }
+
+                BookingQueries.CreateBooking(
+                    (string)body.bookingId,
+                    userID,
+                    email,
+                    (int)body.viewingId
+                );
+
+                var booking = BookingQueries.GetBooking((string)body.bookingId);
+                int bookingId = (int)booking.id;
+
+                BookingQueries.CreateSeats(bookingId, (List<string>body.seats));
+                BookingQueries.CreateTickets(bookingId, body.counts);
+
+                return RestResult.Parse(context, new
+                {
+                    success = true,
+                    bookingReference = (string)body.bookingId
+                });
+            }
             var body = JSON.Parse(bodyJson.ToString());
             body.Delete("id");
             var parsed = ReqBodyParse(table, body);
