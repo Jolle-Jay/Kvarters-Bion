8d080f18fc321f2e1eb8a6f79ff5bddeb76fa9c3
Chim-Cham <gustavfransson@hotmail.com>
2026-02-25 15:45:38 +0100

    Revert "merged repo dev with local"
    This reverts commit 95525a7348b7d2252aaf707d636434d02f54196e, reversing
    changes made to 6a34194254543b12876f2120009c3ccfda98d24e.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { useLocation } from 'react-router-dom';
 import Header from "./partials/Header";
 import Main from './partials/Main';
 import Footer from './partials/Footer';
-import CookieBanner from './partials/Cookie';
 
 
 // turn off when not needed for debugging
@@ -18,7 +17,6 @@ export default function App() {
     <Header />
     <Main />
     <Footer />
-    <CookieBanner />
 
   </>;
 };
