98a16494201f9f6f140a1360c0747258f71efdb5
jenjira185 <jenjira.h04@gmail.com>
2026-03-05 15:16:56 +0100

    Add a comment
    Removed VIP and Pensionär seats since it is onödit to have right now

--- src/pages/BookingPage.tsx
+++ src/pages/BookingPage.tsx
@@ -486,13 +486,7 @@ function BookingPage() {
                   {Array.from({ length: numSeats }, (_, i) => {
                     const col = i + 1;
                     const seatId = `${row}-${col}`;
-                    let seatType: 'available' | 'vip' | 'elder' | 'unavailable' = 'available';
-
-                    if (row === 5 && col >= 4 && col <= 7) {
-                      seatType = 'vip';
-                    } else if (row === 3 && col >= 1 && col <= 3) {
-                      seatType = 'elder';
-                    }
+                    let seatType: 'available'| 'unavailable' = 'available';
 
                     return (
                       <Seat
