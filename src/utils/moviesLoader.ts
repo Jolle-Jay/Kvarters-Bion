eb5bc387e0279a5ca26db5e50a515b7e315f089b
Piexgear <davidpuscas@live.se>
2026-03-17 13:12:18 +0100

    needed to test if the helper and loader made any differance but it did not so i remover the

--- src/utils/moviesLoader.ts
+++
@@ -1,8 +0,0 @@
-export default async function moviesLoader({ params }: any) {
-  let url = '/api/movies';
-  if (params.slug) { url += '?slug=' + params.slug; }
-  return {
-    movies:
-      await (await fetch(url)).json()
-  };
-};
